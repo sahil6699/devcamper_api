5bf9db3aab94e03d3e2044eb17794cd93b2b64f3
sahil6699 <sahilaneja66199@gmail.com>
2022-12-28 16:40:14 +0530

    basic get request for sending html content

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from the world of express</h1>');
+});
+
 const PORT = process.env.PORT || 5000;
+('');
+
 app.listen(
   PORT,
   console.log(
