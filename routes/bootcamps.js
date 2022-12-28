e6b5a0c1196b4bdf6f648150c383997d6ee66fe1
sahil6699 <sahilaneja66199@gmail.com>
2022-12-28 18:14:16 +0530

    set up routes for bootcamps
    -> created routes for bootcamps.
    1) create a folder routes
    2) create a file bootcamps.js
    3. const router = require('express').Router();
    4) replace app with router
    5) export this file with name router
    6) import in server.js
    7) app.use('/api/v1/bootcamps', bootcampRouter);
    8) replace /api/v1/bootcamps with / in routes

---
+++ routes/bootcamps.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+router.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'show all bootcamps',
+  });
+});
+router.get('/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Show bootcamp ${req.params.id}`,
+  });
+});
+router.delete('/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `delete bootcamp ${req.params.id}`,
+  });
+});
+router.put('/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `update bootcamp ${req.params.id}`,
+  });
+});
+router.post('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'create new bootcamp',
+  });
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcampRouter = require('./routes/bootcamps');
+
 //Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from the world of express</h1>');
-});
+app.use(express.json());
+
+//Mount router
+app.use('/api/v1/bootcamps', bootcampRouter);
 
 const PORT = process.env.PORT || 5000;
-('');
 
 app.listen(
   PORT,
