8f8a22b3f823dda74b8752fc679d95e8a962029a
sahil6699 <sahilaneja66199@gmail.com>
2022-12-28 20:12:56 +0530

    trying third party morgan logger

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const dotenv = require('dotenv');
 //Route files
 const bootcampRouter = require('./routes/bootcamps');
 const logger = require('./middleware/logger');
-
+const morgan = require('morgan');
 //Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+// DEV Loggin  middleware
+// app.use(logger);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('combined'));
+}
 
 app.use(express.json());
 
