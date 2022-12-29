fe1f5765a12ed30b13838e0da87d9743874fc48e
sahil6699 <sahilaneja66199@gmail.com>
2022-12-29 07:09:43 +0530

    connecting the mongdb database using mongoose

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+//Load env variables
+dotenv.config({ path: './config/config.env' });
+
+//connect to DB
+connectDB();
 
 //Route files
 const bootcampRouter = require('./routes/bootcamps');
-const logger = require('./middleware/logger');
-const morgan = require('morgan');
-//Load env variables
-dotenv.config({ path: './config/config.env' });
+// const logger = require('./middleware/logger');
 
 const app = express();
 
@@ -23,9 +28,15 @@ app.use('/api/v1/bootcamps', bootcampRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
