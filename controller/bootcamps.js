772afddd5fdc6c4e949ee1dcfdfe40ae788d6d9a
sahil6699 <sahilaneja66199@gmail.com>
2023-01-03 21:43:31 +0530

    basic pagination

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -15,7 +15,7 @@ const getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   //Fields to exclude
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   //Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param]);
@@ -45,6 +45,13 @@ const getBootcamps = asyncHandler(async (req, res, next) => {
   } else {
     query = query.sort('-createdAt');
   }
+
+  //Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const skip = (page - 1) * limit;
+
+  query.skip(skip).limit(limit);
 
   //Executing the query
   const bootcamps = await query;
