2cb6bd9182bd7e8f72c7b8a539393e1f7cfc3444
sahil6699 <sahilaneja66199@gmail.com>
2023-01-23 17:42:19 +0530

    commenting not required code in asynchandler

--- middleware/async.js
+++ middleware/async.js
@@ -1,10 +1,11 @@
 const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
-const asyncWrapper = function (fn) {
-  function someOne(req, res, next) {
-    return Promise.resolve(fn(req, res, next)).catch(next);
-  }
-};
+// simpler way to write the above function
+// const asyncWrapper = function (fn) {
+//   function someOne(req, res, next) {
+//     return Promise.resolve(fn(req, res, next)).catch(next);
+//   }
+// };
 
 module.exports = asyncHandler;
