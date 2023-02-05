2ab623ccc453f5cc1d1b410dff2a4f3255c6cb5c
sahil6699 <sahilaneja66199@gmail.com>
2023-02-05 15:09:45 +0530

    feature: changes in bootcamp & add user relationship

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -16,6 +16,22 @@ const getBootcamps = asyncHandler(async (req, res, next) => {
 //@route POST /api/v1/bootcamps/
 // @access Private
 const createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // if the user is not an admin they can only publish a single bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with id ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({
     success: true,
